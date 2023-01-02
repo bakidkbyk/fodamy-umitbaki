a546c4ecc5792e68ad0fecf3ed9ea061ec22710b
bakidkbyk <umitbaki.dikbiyik@gmail.com>
2023-01-02 16:59:10 +0300

    add tinyconstraint and mobillium builders

--- UIComponents/UIComponents/UIComponents.h
+++ UIComponents/UIComponents/UIComponents.h
@@ -7,6 +7,9 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#import <MobilliumBuilders/MobilliumBuilders-Swift.h>
+#import <TinyConstraints/TinyConstraints-Swift.h>
+
 //! Project version number for UIComponents.
 FOUNDATION_EXPORT double UIComponentsVersionNumber;
 
--- fodamy-baki/ViewController.swift
+++ fodamy-baki/ViewController.swift
@@ -9,5 +9,5 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
+  }
 }
