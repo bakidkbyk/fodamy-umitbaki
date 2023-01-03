2d370341042b49e8b82cf0f41a7eef6e239810e3
bakidkbyk <umitbaki.dikbiyik@gmail.com>
2023-01-03 09:59:22 +0300

    setup data provider and set alomofire pod

--- fodamy-baki/ViewController.swift
+++ fodamy-baki/ViewController.swift
@@ -7,7 +7,11 @@
 
 class ViewController: UIViewController {
     
+    private let denemee = Denemee()
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(denemee)
+        denemee.centerInSuperview()
+        denemee.size(.init(width: 200, height: 200))
   }
 }
