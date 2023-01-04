b979753594598771eae47ee389b074a9c33770e3
bakidkbyk <umitbaki.dikbiyik@gmail.com>
2023-01-04 09:59:32 +0300

    add closures

---
+++ Utilities/Utilities/Constants/Closures.swift
@@ -0,0 +1,9 @@
+//
+//  Closures.swift
+//  Utilities
+//
+//  Created by Baki Dikbıyık on 4.01.2023.
+//
+
+public typealias VoidClosure   = (() -> Void)
+public typealias StringClosure = ((String) -> Void)
