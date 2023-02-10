d9d5273b7c5ae3d77bf67164533addaa49950651
Ümit Baki Dikbıyık <umitbaki.dkbyk@gmail.com>
2023-02-10 17:05:25 +0300

    set swiftgen design rules

--- UIComponents/UIComponents/Extensions/Segmentio+Extensions .swift
+++ UIComponents/UIComponents/Extensions/Segmentio+Extensions .swift
@@ -14,24 +14,26 @@ public extension SegmentioOptions {
         let horizontalSeperatorOptions = SegmentioHorizontalSeparatorOptions(type: .bottom, height: 0, color: .appWhite)
         let verticalSeparatorOptions = SegmentioVerticalSeparatorOptions(ratio: 1, color: .appZircon)
         
-        let segmentStates = SegmentioStates(defaultState: SegmentioState(backgroundColor:
-                .appWhite,
-                                                                    titleFont: .font(.nunitoBold,    size: .medium),
-                                                                    titleTextColor: .appCinder,      titleAlpha: 1),
-                                                                    selectedState: SegmentioState(backgroundColor: .appWhite, titleFont: .font(.nunitoBold, size: .medium),
-                                                                    titleTextColor: .appRed,
-                                                                    titleAlpha: 1),
-                                                                    highlightedState:               SegmentioState(backgroundColor: .appWhite,
-                                                                    titleFont: .font(.nunitoBold,    size: .medium),                  titleTextColor: .appCinder,      titleAlpha: 1))
+        let segmentStates = SegmentioStates(defaultState: SegmentioState(backgroundColor: .appWhite,
+                                                                         titleFont: .font(.nunitoBold, size: .medium),
+                                                                         titleTextColor: .appCinder, titleAlpha: 1),
+                                            selectedState: SegmentioState(backgroundColor: .appWhite,
+                                                                          titleFont: .font(.nunitoBold, size: .medium),
+                                                                          titleTextColor: .appRed, titleAlpha: 1),
+                                            highlightedState: SegmentioState(backgroundColor: .appWhite,
+                                                                              titleFont: .font(.nunitoBold, size: .medium),
+                                                                              titleTextColor: .appCinder, titleAlpha: 1))
         
-        let  segmentioOptions = SegmentioOptions(backgroundColor:
-                .appWhite,
-                                                 segmentPosition: .fixed(maxVisibleItems: 2), scrollEnabled: true,
-                                                 indicatorOptions: indicatorOptions, horizontalSeparatorOptions: horizontalSeperatorOptions, verticalSeparatorOptions: verticalSeparatorOptions,
+        let  segmentioOptions = SegmentioOptions(backgroundColor: .appWhite,
+                                                 segmentPosition: .fixed(maxVisibleItems: 2),
+                                                 scrollEnabled: true,
+                                                 indicatorOptions: indicatorOptions,
+                                                 horizontalSeparatorOptions: horizontalSeperatorOptions,
+                                                 verticalSeparatorOptions: verticalSeparatorOptions,
                                                  labelTextAlignment: .center,
-                                                 segmentStates: segmentStates)
+                                                 segmentStates:
+                                                 segmentStates)
         
         return segmentioOptions
     }
 }
-
