bde9fae032ea4b45766d3f3917d52633367ee8d6
Ümit Baki Dikbıyık <umitbaki.dkbyk@gmail.com>
2023-02-17 10:31:46 +0300

    add userView

---
+++ UIComponents/UIComponents/UIView/UserView.swift
@@ -0,0 +1,79 @@
+//
+//  File.swift
+//  UIComponents
+//
+//  Created by Baki Dikbıyık on 16.02.2023.
+//
+
+public class UserView: UIView {
+    
+    private let userImageView = UIImageViewBuilder()
+        .contentMode(.scaleToFill)
+        .cornerRadius(25)
+        .clipsToBounds(true)
+        .build()
+    
+    private let userInfoStackView = UIStackViewBuilder()
+        .axis(.vertical)
+        .build()
+    
+    private let usernameLabel = UILabelBuilder()
+        .font(.font(.nunitoBold, size: .medium))
+        .textColor(.appZircon)
+        .build()
+    
+    private let recipeCountAndFollowersLabel = UILabelBuilder()
+        .font(.font(.nunitoBold, size: .medium))
+        .textColor(.appHeather)
+        .build()
+    
+    private let followButtonStackView = UIStackViewBuilder()
+        .axis(.vertical)
+        .build()
+    
+    private lazy var followButton = ButtonFactory.createPrimaryBorderedButton(style: .small)
+    
+    public var  isShowsFollowButton = false {
+        didSet {
+            followButton.isHidden = !isShowsFollowButton
+        }
+    }
+    
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubviews()
+    }
+    
+    // swiftlint:disable fatal_error unavailable_function
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    // swiftlint:enable fatal_error unavailable_function
+}
+
+// MARK: - UILayout
+extension UserView {
+    
+    private func addSubviews() {
+        backgroundColor = .appWhite
+        addSubview(userImageView)
+        userImageView.edgesToSuperview(excluding: .trailing, insets: .init(top: 15, left: 15, bottom: 15, right: 15))
+        userImageView.size(.init(width: 40, height: 40))
+        
+        addSubview(userInfoStackView)
+        userInfoStackView.leadingToTrailing(of: userImageView).constant = 10
+        userInfoStackView.centerYToSuperview()
+        
+        userInfoStackView.addArrangedSubview(usernameLabel)
+        userInfoStackView.addArrangedSubview(recipeCountAndFollowersLabel)
+        
+        addSubview(followButtonStackView)
+        followButtonStackView.trailingToSuperview().constant = -15
+        followButtonStackView.leadingToTrailing(of: userInfoStackView).constant = 15
+        followButtonStackView.centerYToSuperview()
+        followButtonStackView.width(120)
+        followButtonStackView.addArrangedSubview(followButton)
+        
+    }
+}
+
