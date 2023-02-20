a7d80d53e027dedd2fc75b681c0cfbfc80176acf
Ümit Baki Dikbıyık <umitbaki.dkbyk@gmail.com>
2023-02-20 13:47:52 +0300

    add conveinence init

---
+++ fodamy-baki/UIComponents/RecipeCellModel+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  RecipeCellModel+Extension.swift
+//  fodamy-baki
+//
+//  Created by Baki Dikbıyık on 20.02.2023.
+//
+
+extension RecipeCellModel {
+    
+    convenience init(recipe: Recipe) {
+        self.init(recipeId: recipe.id,
+                  userId: recipe.user.id,
+                  usernameImageUrl: (recipe.user.image.url)!,
+                  username: recipe.user.username,
+                  recipeAndFollowers: "\(recipe.user.recipeCount) Tarif \(recipe.user.followingCount) Takipçi",
+                  recipeTitle: recipe.title,
+                  categoryName: recipe.category.name,
+                  foodImageUrl: (recipe.image.first?.url)!,
+                  commentAndLikes: "\(recipe.commentCount) Yorum \(recipe.likeCount) Beğeni")
+    }
+}
