b1ddfd1c90229598c158e829f541bcc23823d639
Ümit Baki Dikbıyık <umitbaki.dkbyk@gmail.com>
2023-07-19 15:01:29 +0300

    add notification extension name

---
+++ UIComponents/UIComponents/Extensions/NotificationName+Extension.swift
@@ -0,0 +1,10 @@
+//
+//  NotificationName+Extension.swift
+//  UIComponents
+//
+//  Created by Baki Dikbıyık on 19.07.2023.
+//
+
+public extension Notification.Name {
+    static let updateDetailView = Notification.Name(rawValue: "updateDetailView")
+}
